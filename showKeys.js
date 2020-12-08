098197c18cc03596154135300a4db4ab96f943c3
xxninjabunnyxx <xxnninjabunnyxx@gmail.com>
2020-12-08 16:50:07 -0500

    finished reinforcement cards

--- showKeys.js
+++ showKeys.js
@@ -1,20 +1,20 @@
 const fs = require('fs')
-const data = fs.readdirSync('./clow')
+const data = fs.readdirSync('./reinforcement')
 
 const showKeys = () => {
     let cards = data.map(card => {
-        return Object.keys(JSON.parse(fs.readFileSync(`./clow/${card}`, 'utf8')))
+        return Object.keys(JSON.parse(fs.readFileSync(`./reinforcement/${card}`, 'utf8')))
     })
     console.log(new Set(cards.flat()))
 }
 
 const editKeys = () => {
     let cards = data.map(async card => {
-        cardData = await JSON.parse(fs.readFileSync(`./clow/${card}`, 'utf8'))
-        if(cardData.hasOwnProperty('Major Clow Spirit')){
-            cardData.major_clow_spirit = card['Major Clow Spirit']
-            delete cardData['Major Clow Spirit']
-            fs.writeFileSync(`./clow/${card}`, JSON.stringify(cardData))
+        cardData = await JSON.parse(fs.readFileSync(`./reinforcement/${card}`, 'utf8'))
+        if(cardData.hasOwnProperty('Character type:')){
+            cardData.character_type = card['Character type:']
+            delete cardData['Character type:']
+            fs.writeFileSync(`./reinforcement/${card}`, JSON.stringify(cardData))
         }
         return card
     })
@@ -24,22 +24,23 @@ const editKeys = () => {
 const nomalizeKeys = () => {
     const keys =  {
         name: "",
+        character_type: "",
         number: "",
         rarity: "",
         card_type: "",
-        element:"",
         set_name: "",
-        major_clow_spirit: false
+        effect: ""
+      
     }
 
     let  cards = data.map(async card => {
-        cardData = await JSON.parse(fs.readFileSync(`./clow/${card}`, 'utf8'))
+        cardData = await JSON.parse(fs.readFileSync(`./reinforcement/${card}`, 'utf8'))
         for (let key in keys) {
             if (!cardData.hasOwnProperty(key)) {
                 cardData[key] = keys[key]
                 console.log(cardData)
 
-                fs.writeFileSync(`./clow/${card}`, JSON.stringify(cardData))
+                fs.writeFileSync(`./reinforcement/${card}`, JSON.stringify(cardData))
             }
           }
     })
@@ -47,4 +48,4 @@ const nomalizeKeys = () => {
 }
 //showKeys()
 //editKeys()
-nomalizeKeys()
+//nomalizeKeys()
