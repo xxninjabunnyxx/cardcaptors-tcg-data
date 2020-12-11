4b0fa2058800f38c9294e4ed2cb150b565088cc8
xxninjabunnyxx <xxnninjabunnyxx@gmail.com>
2020-12-11 17:19:06 -0500

    finished strategy

--- yaml.js
+++ yaml.js
@@ -1,10 +1,10 @@
 const yaml = require('yaml')
 const fs = require('fs')
-const cards = fs.readdirSync('./reinforcement')
+const cards = fs.readdirSync('./strategy')
 
 const yamlConvert = () => {
     cards.map(async card => {
-        let cardData = await JSON.parse(fs.readFileSync(`./reinforcement/${card}`, `utf-8`))
+        let cardData = await JSON.parse(fs.readFileSync(`./strategy/${card}`, `utf-8`))
         let cardDataYml = await yaml.stringify(cardData)
         fs.writeFileSync(`./cards/${cardData.number}.yml`, cardDataYml)
     })
